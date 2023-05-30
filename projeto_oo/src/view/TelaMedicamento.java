8dce31bf7be1b51e04c64f033c3ee244dcfcdb7d
Caio Pacheco <caiopachecos29@gmail.com>
2023-05-30 13:47:32 -0300

    TelaMedicamento criada

---
+++ projeto_oo/src/view/TelaMedicamento.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.awt.Font;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import controle.*;
+
+public class TelaMenu implements ActionListener, ListSelectionListener {
+    private static JFrame janelaMedicamento = new JFrame("Medicamento");
+    private static JButton salvar = new JButton("Salvar");
+    private static JTextField nomeMedicamento = new JTextField ("Sertralina");
+
+}
