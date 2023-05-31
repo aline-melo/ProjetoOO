eef0d104aaa63cee7963f689caeee818ec85b517
Caio Pacheco <caiopachecos29@gmail.com>
2023-05-31 12:19:13 -0300

    Tela Medicamento com todos os elementos

--- projeto_oo/src/view/TelaMedicamento.java
+++ projeto_oo/src/view/TelaMedicamento.java
@@ -12,45 +12,69 @@ import controle.*;
 public class TelaMedicamento implements ActionListener, ListSelectionListener {
     private static JFrame janelaMedicamento = new JFrame("Medicamento");
     private static JButton salvar = new JButton("Salvar");
-    private static JTextField nomeMedicamento = new JTextField ("Nome do medicamento");
-    private static JTextField atributo1 = new JTextField ("atributo1");
-    private static JTextField atributo2 = new JTextField ("atributo2");
-    private static JTextField atributo3 = new JTextField ("atributo3");
-    private static JTextField atributo4 = new JTextField ("atributo4");
-    private static JTextField atributo5 = new JTextField ("atributo5");
-    private static JTextField atributo6 = new JTextField ("atributo6");
-    private static JTextField atributo7 = new JTextField ("atributo7");
-    private static JTextField atributo8 = new JTextField ("atributo8");
-
+    private static JTextField nomeMedicamento = new JTextField("Nome do medicamento");
+    private static JTextField field_atributo1 = new JTextField("field_atributo1");
+    private static JTextField field_atributo2 = new JTextField("field_atributo2");
+    private static JTextField field_atributo3 = new JTextField("field_atributo3");
+    private static JTextField field_atributo4 = new JTextField("field_atributo4");
+    private static JTextField field_atributo5 = new JTextField("field_atributo5");
+    private static JTextField field_atributo6 = new JTextField("field_atributo6");
+    private static JTextField field_atributo7 = new JTextField("field_atributo7");
+    private static JTextField field_atributo8 = new JTextField("field_atributo8");
+    private static JLabel label_atributo1 = new JLabel("label_atributo1");
+    private static JLabel label_atributo2 = new JLabel("label_atributo2");
+    private static JLabel label_atributo3 = new JLabel("label_atributo3");
+    private static JLabel label_atributo4 = new JLabel("label_atributo4");
+    private static JLabel label_atributo5 = new JLabel("label_atributo5");
+    private static JLabel label_atributo6 = new JLabel("label_atributo6");
+    private static JLabel label_atributo7 = new JLabel("label_atributo7");
+    private static JLabel label_atributo8 = new JLabel("label_atributo8");
 
 
     public TelaMedicamento() {
         nomeMedicamento.setBounds(120, 50, 300, 30);
         salvar.setBounds(500, 50, 100, 30);
-        atributo1.setBounds(120, 100, 100, 30);
-        atributo2.setBounds(120, 150, 100, 30);
-        atributo3.setBounds(120, 200, 100, 30);
-        atributo4.setBounds(120, 250, 100, 30);
-        atributo5.setBounds(220, 100, 100, 30);
-        atributo6.setBounds(220, 150, 100, 30);
-        atributo7.setBounds(220, 200, 100, 30);
-        atributo8.setBounds(220, 250, 100, 30);
+        field_atributo1.setBounds(120, 100, 100, 30);
+        field_atributo2.setBounds(120, 150, 100, 30);
+        field_atributo3.setBounds(120, 200, 100, 30);
+        field_atributo4.setBounds(120, 250, 100, 30);
+        field_atributo5.setBounds(220, 100, 100, 30);
+        field_atributo6.setBounds(220, 150, 100, 30);
+        field_atributo7.setBounds(220, 200, 100, 30);
+        field_atributo8.setBounds(220, 250, 100, 30);
+
+        label_atributo1.setBounds(120, 75, 100, 25);
+        label_atributo2.setBounds(120, 125, 100, 25);
+        label_atributo3.setBounds(120, 175, 100, 25);
+        label_atributo4.setBounds(120, 225, 100, 25);
+        label_atributo5.setBounds(220, 75, 100, 25);
+        label_atributo6.setBounds(220, 125, 100, 25);
+        label_atributo7.setBounds(220, 175, 100, 25);
+        label_atributo8.setBounds(220, 225, 100, 25);
 
         janelaMedicamento.setLayout(null);
         janelaMedicamento.add(nomeMedicamento);
-        janelaMedicamento.add(atributo1);
-        janelaMedicamento.add(atributo2);
-        janelaMedicamento.add(atributo3);
-        janelaMedicamento.add(atributo4);
-        janelaMedicamento.add(atributo5);
-        janelaMedicamento.add(atributo6);
-        janelaMedicamento.add(atributo7);
-        janelaMedicamento.add(atributo8);
+        janelaMedicamento.add(field_atributo1);
+        janelaMedicamento.add(field_atributo2);
+        janelaMedicamento.add(field_atributo3);
+        janelaMedicamento.add(field_atributo4);
+        janelaMedicamento.add(field_atributo5);
+        janelaMedicamento.add(field_atributo6);
+        janelaMedicamento.add(field_atributo7);
+        janelaMedicamento.add(field_atributo8);
+        janelaMedicamento.add(label_atributo1);
+        janelaMedicamento.add(label_atributo2);
+        janelaMedicamento.add(label_atributo3);
+        janelaMedicamento.add(label_atributo4);
+        janelaMedicamento.add(label_atributo5);
+        janelaMedicamento.add(label_atributo6);
+        janelaMedicamento.add(label_atributo7);
+        janelaMedicamento.add(label_atributo8);
         janelaMedicamento.add(salvar);
 
         janelaMedicamento.setSize(700, 500);
         janelaMedicamento.setVisible(true);
-   }
+    }
     public static void main(String [] args) {
         TelaMedicamento tela = new TelaMedicamento();
     }
