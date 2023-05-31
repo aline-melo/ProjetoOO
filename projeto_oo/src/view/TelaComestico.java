53a6ff8140f8ad9472f112a9fffcf5b9d3770a34
Caio Pacheco <caiopachecos29@gmail.com>
2023-05-31 16:49:25 -0300

    TelaMedicamento e TelaComestico atualizadas

--- projeto_oo/src/view/TelaComestico.java
+++ projeto_oo/src/view/TelaComestico.java
@@ -8,8 +8,8 @@ import java.awt.event.ActionListener;
 
 public class TelaComestico implements ActionListener, ListSelectionListener {
     private static JFrame janelaComestico = new JFrame("Cosmetico");
-    private static JButton salvar = new JButton("Salvar");
-    private static JTextField nomeCosmetico = new JTextField("Nome do comestico");
+    private static JButton button_salvar = new JButton("Salvar");
+    private static JTextField field_nome = new JTextField("Nome do comestico");
     private static JTextField field_descricao = new JTextField("field_descricao");
     private static JTextField field_preco = new JTextField("field_preco");
     private static JTextField field_estoque = new JTextField("field_estoque");
@@ -29,28 +29,28 @@ public class TelaComestico implements ActionListener, ListSelectionListener {
 
 
     public TelaComestico() {
-        nomeCosmetico.setBounds(120, 50, 300, 30);
-        salvar.setBounds(500, 50, 100, 30);
-        field_descricao.setBounds(120, 100, 100, 30);
-        field_preco.setBounds(120, 150, 100, 30);
-        field_estoque.setBounds(120, 200, 100, 30);
-        field_fabricante.setBounds(120, 250, 100, 30);
-        field_tamanho_embalagem.setBounds(220, 100, 100, 30);
-        field_cor.setBounds(220, 150, 100, 30);
-        field_fragancia.setBounds(220, 200, 100, 30);
-        checkbox_hipoalergenico.setBounds(220, 250, 100, 30);
+        field_nome.setBounds(120, 50, 300, 30);
+        button_salvar.setBounds(500, 50, 100, 30);
+        field_descricao.setBounds(120, 99, 180, 30);
+        field_preco.setBounds(120, 150, 180, 30);
+        field_estoque.setBounds(120, 200, 180, 30);
+        field_fabricante.setBounds(120, 250, 180, 30);
+        field_tamanho_embalagem.setBounds(320, 99, 180, 30);
+        field_cor.setBounds(320, 150, 180, 30);
+        field_fragancia.setBounds(320, 200, 180, 30);
+        checkbox_hipoalergenico.setBounds(320, 250, 180, 30);
 
-        label_descricao.setBounds(120, 75, 100, 25);
-        label_preco.setBounds(120, 125, 100, 25);
-        label_estoque.setBounds(120, 175, 100, 25);
-        label_fabricante.setBounds(120, 225, 100, 25);
-        label_tamanho_embalagem.setBounds(220, 75, 100, 25);
-        label_cor.setBounds(220, 125, 100, 25);
-        label_fragancia.setBounds(220, 175, 100, 25);
-        label_hipoalergenico.setBounds(220, 225, 100, 25);
+        label_descricao.setBounds(120, 75, 180, 25);
+        label_preco.setBounds(120, 125, 180, 25);
+        label_estoque.setBounds(120, 175, 180, 25);
+        label_fabricante.setBounds(120, 225, 180, 25);
+        label_tamanho_embalagem.setBounds(320, 75, 180, 25);
+        label_cor.setBounds(320, 125, 180, 25);
+        label_fragancia.setBounds(320, 175, 180, 25);
+        label_hipoalergenico.setBounds(320, 225, 180, 25);
 
         janelaComestico.setLayout(null);
-        janelaComestico.add(nomeCosmetico);
+        janelaComestico.add(field_nome);
         janelaComestico.add(field_descricao);
         janelaComestico.add(field_preco);
         janelaComestico.add(field_estoque);
@@ -68,7 +68,7 @@ public class TelaComestico implements ActionListener, ListSelectionListener {
         janelaComestico.add(label_cor);
         janelaComestico.add(label_fragancia);
         janelaComestico.add(label_hipoalergenico);
-        janelaComestico.add(salvar);
+        janelaComestico.add(button_salvar);
 
         janelaComestico.setSize(700, 500);
         janelaComestico.setVisible(true);
--- projeto_oo/src/view/TelaMedicamento.java
+++ projeto_oo/src/view/TelaMedicamento.java
@@ -1,77 +1,79 @@
 package view;
 
-import java.awt.Font;
 import java.awt.event.*;
-import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-import controle.*;
-
 public class TelaMedicamento implements ActionListener, ListSelectionListener {
     private static JFrame janelaMedicamento = new JFrame("Medicamento");
-    private static JButton salvar = new JButton("Salvar");
-    private static JTextField nomeMedicamento = new JTextField("Nome do medicamento");
+    private static JButton button_salvar = new JButton("Salvar");
+    private static JTextField field_nome = new JTextField("Nome do medicamento");
     private static JTextField field_descricao = new JTextField("field_descricao");
     private static JTextField field_preco = new JTextField("field_preco");
     private static JTextField field_estoque = new JTextField("field_estoque");
     private static JTextField field_fabricante = new JTextField("field_fabricante");
     private static JTextField field_tamanho_embalagem = new JTextField("field_tamanho_embalagem");
-    private static JTextField field_atributo6 = new JTextField("field_atributo6");
-    private static JTextField field_atributo7 = new JTextField("field_atributo7");
-    private static JTextField field_atributo8 = new JTextField("field_atributo8");
-    private static JLabel label_descricao = new JLabel("label_descricao");
-    private static JLabel label_preco = new JLabel("label_preco");
-    private static JLabel label_estoque = new JLabel("label_estoque");
-    private static JLabel label_fabricante = new JLabel("label_fabricante");
-    private static JLabel label_tamanho_embalagem = new JLabel("label_tamanho_embalagem");
-    private static JLabel label_atributo6 = new JLabel("label_atributo6");
-    private static JLabel label_atributo7 = new JLabel("label_atributo7");
-    private static JLabel label_atributo8 = new JLabel("label_atributo8");
+    private static JTextField field_tratamento = new JTextField("field_tratamento");
+    private static JTextField field_tarja = new JTextField("field_tarja");
+    private static JTextField field_principio_ativo = new JTextField("field_principio_ativo");
+    private static JCheckBox checkbox_generico = new JCheckBox("Sim");
+    private static JLabel label_descricao = new JLabel("Descrição:");
+    private static JLabel label_preco = new JLabel("Preço:");
+    private static JLabel label_estoque = new JLabel("Quantidade em Estoque:");
+    private static JLabel label_fabricante = new JLabel("Fabricante:");
+    private static JLabel label_tamanho_embalagem = new JLabel("Tamanho:");
+    private static JLabel label_tratamento = new JLabel("Tipo de Tratamento:");
+    private static JLabel label_tarja = new JLabel("Tarja:");
+    private static JLabel label_principio_ativo = new JLabel("Princípios Ativos:");
+    private static JLabel label_generico = new JLabel("É genérico?");
 
 
     public TelaMedicamento() {
-        nomeMedicamento.setBounds(120, 50, 300, 30);
-        salvar.setBounds(500, 50, 100, 30);
+        field_nome.setBounds(120, 50, 300, 30);
+        button_salvar.setBounds(500, 50, 100, 30);
         field_descricao.setBounds(120, 100, 100, 30);
         field_preco.setBounds(120, 150, 100, 30);
         field_estoque.setBounds(120, 200, 100, 30);
         field_fabricante.setBounds(120, 250, 100, 30);
         field_tamanho_embalagem.setBounds(220, 100, 100, 30);
-        field_atributo6.setBounds(220, 150, 100, 30);
-        field_atributo7.setBounds(220, 200, 100, 30);
-        field_atributo8.setBounds(220, 250, 100, 30);
+        field_tratamento.setBounds(220, 150, 100, 30);
+        field_tarja.setBounds(220, 200, 100, 30);
+        field_principio_ativo.setBounds(220, 250, 100, 30);
+        checkbox_generico.setBounds(120, 300, 100, 30);
 
         label_descricao.setBounds(120, 75, 100, 25);
         label_preco.setBounds(120, 125, 100, 25);
         label_estoque.setBounds(120, 175, 100, 25);
         label_fabricante.setBounds(120, 225, 100, 25);
         label_tamanho_embalagem.setBounds(220, 75, 100, 25);
-        label_atributo6.setBounds(220, 125, 100, 25);
-        label_atributo7.setBounds(220, 175, 100, 25);
-        label_atributo8.setBounds(220, 225, 100, 25);
+        label_tratamento.setBounds(220, 125, 100, 25);
+        label_tarja.setBounds(220, 175, 100, 25);
+        label_principio_ativo.setBounds(220, 225, 100, 25);
+        label_generico.setBounds(120, 275, 100, 25);
 
         janelaMedicamento.setLayout(null);
-        janelaMedicamento.add(nomeMedicamento);
+        janelaMedicamento.add(field_nome);
         janelaMedicamento.add(field_descricao);
         janelaMedicamento.add(field_preco);
         janelaMedicamento.add(field_estoque);
         janelaMedicamento.add(field_fabricante);
         janelaMedicamento.add(field_tamanho_embalagem);
-        janelaMedicamento.add(field_atributo6);
-        janelaMedicamento.add(field_atributo7);
-        janelaMedicamento.add(field_atributo8);
+        janelaMedicamento.add(field_tratamento);
+        janelaMedicamento.add(field_tarja);
+        janelaMedicamento.add(field_principio_ativo);
+        janelaMedicamento.add(checkbox_generico);
 
         janelaMedicamento.add(label_descricao);
         janelaMedicamento.add(label_preco);
         janelaMedicamento.add(label_estoque);
         janelaMedicamento.add(label_fabricante);
         janelaMedicamento.add(label_tamanho_embalagem);
-        janelaMedicamento.add(label_atributo6);
-        janelaMedicamento.add(label_atributo7);
-        janelaMedicamento.add(label_atributo8);
-        janelaMedicamento.add(salvar);
+        janelaMedicamento.add(label_tratamento);
+        janelaMedicamento.add(label_tarja);
+        janelaMedicamento.add(label_principio_ativo);
+        janelaMedicamento.add(label_generico);
+        janelaMedicamento.add(button_salvar);
 
         janelaMedicamento.setSize(700, 500);
         janelaMedicamento.setVisible(true);
