758601e989affba59622d7e50ec44e269d71ddff
aline-melo <aline1amo@gmail.com>
2023-06-30 12:57:08 -0300

    tieri linhas não necessárias do teste

--- projeto_oo/testes/teste/Testes.java
+++ projeto_oo/testes/teste/Testes.java
@@ -13,16 +13,12 @@ import org.junit.jupiter.api.Test;
 
 	  @Test
 	  void testeListarCidades() {
-		  
-		  ArrayList<Produto> estoque1 = new ArrayList<Produto>();
-		  Loja loja1 = new Loja("local1", "cidade1", estoque1);
 		  String esperado ="cidade1";
+		  Loja loja1 = new Loja("local1",esperado, null);
 		  Dados dadosTeste = new Dados();
 		  dadosTeste.getLojas().add(loja1);
+		  
 		  String resultado = dadosTeste.listarCidades()[dadosTeste.listarCidades().length-1];
-		  System.out.println(resultado);
-		  
-		  
 		  
 		  assertEquals(resultado,esperado);
 	  }
