eecc32f535719042bd202c4dc2694c067fd65833
aline-melo <aline1amo@gmail.com>
2023-07-02 19:19:49 -0300

    testes de busca mais completos

--- projeto_oo/testes/teste/Testes.java
+++ projeto_oo/testes/teste/Testes.java
@@ -12,14 +12,20 @@ import org.junit.jupiter.api.Test;
   class Testes {
 	  @Test
 	  void testeBuscarLojas () {
-		  Loja loja1 = new Loja("local1","cidade1", null);
 		  Dados dadosTeste = new Dados();
-		  dadosTeste.getLojas().add(loja1);
+		  Loja loja1 = new Loja("local 1","cidade1", null);
+		  Loja loja2 = new Loja("local 2","cidade2", null);
 		  
-		  assertTrue(dadosTeste.buscar_lojas("local1").contains(loja1));
+		  dadosTeste.getLojas().add(loja1);
+		  dadosTeste.getLojas().add(loja2);
+		  
+		  assertTrue(dadosTeste.buscar_lojas("local 1").contains(loja1));
 		  assertTrue(dadosTeste.buscar_lojas("cidade1").contains(loja1));
+		  assertTrue(dadosTeste.buscar_lojas("local").contains(loja1));
+		  assertTrue(dadosTeste.buscar_lojas("local").contains(loja2));
 		  assertTrue(dadosTeste.buscar_lojas("local onde não tem loja").isEmpty());
 		  assertFalse(dadosTeste.buscar_lojas("nomeErrado").contains(loja1));
+		  assertFalse(dadosTeste.buscar_lojas("nomeErrado").contains(loja2));
 	  }
 	  
 	  
@@ -29,13 +35,17 @@ import org.junit.jupiter.api.Test;
 		  Loja loja1 = new Loja("local1","cidade1", new ArrayList<Produto>());
 		  
 		  Cosmetico cosmeticoTeste = new Cosmetico ("produto existente", null, null, 0, 0, null, null, null, false);
+		  Medicamento medicamentoTeste = new Medicamento("outro produto", null, null, 0, 0, null, null, null, false, null);
+		  
 		  
 		  dadosTeste.getLojas().add(loja1);
 		  loja1.addToEstoque(cosmeticoTeste);
-		  
+		  loja1.addToEstoque(medicamentoTeste);
 		  
 		  assertTrue(dadosTeste.buscar_tudo("produto").contains(cosmeticoTeste));
+		  assertTrue(dadosTeste.buscar_tudo("produto").contains(medicamentoTeste));
 		  assertTrue(dadosTeste.buscar_tudo("produto existente").contains(cosmeticoTeste));
+		  assertFalse(dadosTeste.buscar_tudo("produto existente").contains(medicamentoTeste));
 		  assertFalse(dadosTeste.buscar_tudo("nome errado").contains(cosmeticoTeste));
 		  assertTrue(dadosTeste.buscar_tudo("produto inexistente").isEmpty());
 	  }
@@ -46,7 +56,7 @@ import org.junit.jupiter.api.Test;
 		  Loja loja1 = new Loja("local1","cidade1", new ArrayList<Produto>());
 		  
 		  Medicamento medicamentoTeste = new Medicamento(null, null, null, 0, 0, null, null, null, false, null);
-		  Cosmetico cosmeticoTeste = new Cosmetico ("produto existente", null, null, 0, 0, null, null, null, false);
+		  Cosmetico cosmeticoTeste = new Cosmetico (null, null, null, 0, 0, null, null, null, false);
 		  
 		  dadosTeste.getLojas().add(loja1);
 		  loja1.addToEstoque(cosmeticoTeste);
